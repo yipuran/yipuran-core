8e3af1f93a4016f298b525258a8c144a1a6cb63e
yipuran <postyip2@gmail.com>
2019-09-13 19:26:31 +0900

    change InputStrean → Scanner

--- src/main/java/org/yipuran/env/StdKeyUtil.java
+++ src/main/java/org/yipuran/env/StdKeyUtil.java
@@ -1,8 +1,10 @@
 package org.yipuran.env;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * キー入力ユーティリティ
@@ -11,13 +13,17 @@ public final class StdKeyUtil{
 	private StdKeyUtil(){}
 	/**
 	 * 標準キー入力.
-	 * <br>入力制限＝256バイト<br>
-	 * 指定するプロンプト文字列を標準出力した後に標準入力を開始する。
+	 * <PRE>指定するプロンプト文字列を標準出力した後に標準入力を開始する。</PRE>
 	 * @param prompt プロンプト
 	 * @return 標準入力された文字列
 	 * @throws IOException
 	 */
+	@SuppressWarnings("resource")
 	public final static String getKeyin(String prompt) throws IOException{
+		System.out.print(prompt);
+		Scanner scan = new Scanner(System.in);
+		return scan.nextLine();
+		/*
 		int iCount;
 		String rtn="";
 		char cStr[] = new char[256];
@@ -31,5 +37,24 @@ public final class StdKeyUtil{
 		}
 		rtn=sb.toString();
 		return rtn;
+		*/
+	}
+	/**
+	 * 連続キー入力.
+	 * <PRE>
+	 * 文字列 List を順に標準出力 System.out.print 実行して
+	 * 出力した文字列と対になるキー入力の Map を取得する。
+	 * （注意：実行後のプロセスの標準入力 System.in は close する）
+	 * </PRE>
+	 * @param promptList 標準出力する文字列 List
+	 * @return key=標準出力した文字列、value=入力文字列のMap
+	 */
+	public static Map<String, String> getKeyin(List<String> promptList){
+		try(Scanner scan = new Scanner(System.in)){
+			return promptList.stream().collect(()->new HashMap<String, String>(), (r, t)->{
+				System.out.print(t);
+				r.put(t, scan.nextLine());
+			},(r, u)->{});
+		}
 	}
 }
